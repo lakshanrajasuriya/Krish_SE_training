05d1523556b8a9533df404057c5765b896f713e5
Lakshan Rajasuriya <lakshanrajasuriya@gmail.com>
2021-01-26 16:57:48 +0530

    init builder

---
+++ design-patterns/builder-pattern/src/main/java/org/virtusa/domainregistry/domain/DomainTelescopic2.java
@@ -0,0 +1,37 @@
+package org.virtusa.domainregistry.domain;
+
+public class DomainTelescopic2 {
+    String domainName;
+    int numberOfEmail;
+    Boolean ssl;
+    Boolean whoIsGuard;
+
+    public DomainTelescopic2(String domainName, int numberOfEmail, Boolean ssl, Boolean whoIsGuard) {
+        this.domainName = domainName;
+        this.numberOfEmail = numberOfEmail;
+        this.ssl = ssl;
+        this.whoIsGuard = whoIsGuard;
+    }
+
+    public DomainTelescopic2(String domainName, int numberOfEmail, Boolean ssl) {
+        this(domainName, numberOfEmail, ssl, null);
+    }
+
+    public DomainTelescopic2(String domainName, int numberOfEmail) {
+        this(domainName, numberOfEmail, null);
+    }
+
+    public DomainTelescopic2(String domainName) {
+        this(domainName, 0);
+    }
+
+    @Override
+    public String toString() {
+        return "DomainTelescopic2 {" +
+                "domainName='" + domainName + '\'' +
+                ", numberOfEmail=" + numberOfEmail +
+                ", ssl=" + ssl +
+                ", whoIsGuard=" + whoIsGuard +
+                '}';
+    }
+}
