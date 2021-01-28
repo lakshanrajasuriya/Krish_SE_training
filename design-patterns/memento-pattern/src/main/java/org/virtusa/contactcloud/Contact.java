38dd8977ba431efc15af708e796cbb4bc6d95c54
Lakshan Rajasuriya <lakshanrajasuriya@gmail.com>
2021-01-29 00:18:10 +0530

    init memento

---
+++ design-patterns/memento-pattern/src/main/java/org/virtusa/contactcloud/Contact.java
@@ -0,0 +1,22 @@
+package org.virtusa.contactcloud;
+
+public class Contact {
+    String name;
+    String contactNo;
+    String email;
+
+    public Contact(String name, String contactNo, String email) {
+        this.name = name;
+        this.contactNo = contactNo;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact {" +
+                "name='" + name + '\'' +
+                ", contactNo='" + contactNo + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
