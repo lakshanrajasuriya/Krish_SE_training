f6b1ddf06bba321db38b65ba4363504eae5cfe43
Lakshan Rajasuriya <lakshanrajasuriya@gmail.com>
2021-02-10 01:40:39 +0530

    init 012

---
+++ microservices/012-auth-code-flow-implementation/booking-ui/src/main/java/lk/lakshan/booking/bookingui/controller/UIController.java
@@ -0,0 +1,31 @@
+package lk.lakshan.booking.bookingui.controller;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableOAuth2Sso
+public class UIController extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+
+    @RequestMapping(value = "/")
+    public String loadUI(){
+        return "index";
+    }
+
+    @RequestMapping(value = "/checkin")
+    public String loadCheckInUI(){
+        return "checkin";
+    }
+}
